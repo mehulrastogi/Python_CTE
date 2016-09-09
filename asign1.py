ca72922be55d34805829305495e3274698fcc139
mehulrastogi <mehul.rastogi.mehul@gmail.com>
2016-09-09 22:32:56 +0530

    assignment 1 final

---
+++ asign1.py
@@ -0,0 +1,13 @@
+a= "hello"
+
+print a[0:1:]
+print a[0:2:]
+print a[0:3:]
+print a[0:4:]
+print a[0::]
+print a[-1:-2:-1]
+print a[-1:-3:-1]
+print a[-1:-4:-1]
+print a[-1:-5:-1]
+print a[-1::-1]
+
